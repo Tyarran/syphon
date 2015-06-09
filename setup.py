dc62b675d669c0893ab1ca948e5fbdf18186f003
Romain Commandé <commande.romain@gmail.com>
2015-06-10 00:09:49 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='syphon',
+      version=version,
+      description="",
+      long_description="""\
+""",
+      classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Romain Commandé',
+      author_email='commande.romain@gmail.com',
+      url='http://www.rcommande.org',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
